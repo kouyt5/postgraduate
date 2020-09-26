7050af74a894d14442aa183301f0495a38724e8d
kouyt5 <30313488+kouyt5@users.noreply.github.com>
2020-09-27 01:32:04 +0800

    init

---
+++ cource-neural-network/minist/Model.py
@@ -0,0 +1,87 @@
+import torch
+import torch.nn as nn
+
+class LinerModel(nn.Module):
+    def __init__(self,iamge_w:int,image_h:int):
+        """
+        模型输入为(N,1,28,28),线性模型期望输入为(N,L)
+        因此首先需要将输入转化，这里模型仅为线性的组合
+        """
+        super().__init__()
+        self.drop_rate = 0.2
+        self.liner = nn.Sequential(
+            nn.Linear(iamge_w*image_h,1024),
+            nn.Dropout(p=self.drop_rate),
+            nn.PReLU(),
+
+            nn.Linear(1024,2048),
+            nn.Dropout(p=self.drop_rate),
+            nn.ReLU(),
+
+            nn.Linear(2048,1024),
+            nn.Dropout(p=self.drop_rate),
+            nn.ReLU(),
+
+            nn.Linear(1024,512),
+            nn.Dropout(p=self.drop_rate),
+            nn.ReLU(),
+
+            nn.Linear(512,10),
+            nn.Dropout(p=self.drop_rate),
+            nn.ReLU(),
+        )
+
+    def forward(self, x): #(N,1,28,28)
+        trans_x = torch.squeeze(x,1).view([x.size(0),x.size(2)*x.size(3)])
+        out = self.liner(trans_x) #(N,10)
+        # out = nn.Softmax(dim=-1)(out)
+        return out
+
+
+class CnnModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.drop_rate = 0.2
+        self.cnn = nn.Sequential(
+            nn.Conv2d(1,64,3,stride=1,padding=1,dilation=2),
+            nn.BatchNorm2d(64),
+            nn.PReLU(),
+            nn.Dropout(p=self.drop_rate),
+
+            nn.Conv2d(64,256,3,stride=2,padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Dropout(p=self.drop_rate),
+
+            nn.Conv2d(256,512,3,stride=2,padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Dropout(p=self.drop_rate),
+
+            nn.Conv2d(512,512,3,stride=2,padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Dropout(p=self.drop_rate),
+
+            nn.Conv2d(512,256,3,stride=2,padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Dropout(p=self.drop_rate),
+        )
+
+        self.linear = nn.Linear(256*2*2,10)
+
+    def forward(self,x):
+        out = self.cnn(x)
+        out = out.view(out.size(0),out.size(1)*out.size(2)*out.size(3))
+        out = self.linear(out)
+        return out
+
+if __name__ == "__main__":
+    x = torch.randn([8,1,28,28])
+    # model = LinerModel(28,28)
+    model = CnnModel()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    out = model(x.to(device))
+    print()
---
+++ cource-neural-network/minist/main.py
@@ -0,0 +1,99 @@
+import os
+import torch
+import torchvision
+import tqdm
+from Model import LinerModel,CnnModel
+from sklearn import metrics
+
+def get_pair(predict, true):
+    y = torch.argmax(predict, dim=-1)
+    y_list = y.cpu().numpy().tolist()
+    true_list = true.cpu().numpy().tolist()
+    return y_list, true_list
+
+
+def test(model, dataloader):
+    model.eval()
+    predict_list = []
+    true_List = []
+    total_loss = 0.
+    cri = torch.nn.CrossEntropyLoss()
+    for i, batch in enumerate(dataloader):
+        out = model(batch[0].to(device))
+        loss = cri(out, batch[1].to(device))
+        predit, true = get_pair(out, batch[1].to(device))
+        predict_list.extend(predit)
+        true_List.extend(true)
+        total_loss += loss.item()
+    acc = metrics.accuracy_score(predict_list, true_List)
+    print("****test loss"+str('%.3f' % (total_loss/i))+" acc: "+str(acc)+"****")
+    return acc
+
+
+DATA_ROOT = os.path.abspath(os.path.join('cource-neural-network',
+                                         'minist'))  # MINIST 数据集存放位置
+CKECKPOINT_ROOT = os.path.abspath(os.path.join('cource-neural-network',
+                                         'minist','checkpoint'))
+if not os.path.exists(CKECKPOINT_ROOT):
+    os.makedirs(CKECKPOINT_ROOT)
+train_transform = torchvision.transforms.Compose([
+    #torchvision.transforms.RandomCrop((24,24)), #.CenterCrop((24, 24))
+    torchvision.transforms.RandomRotation(45),
+    torchvision.transforms.Resize((28, 28)),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize([0.5], [0.5])
+])
+test_transform = torchvision.transforms.Compose([
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize([0.5], [0.5])
+])
+minist_datasets_train = torchvision.datasets.MNIST(DATA_ROOT, train=True,
+                                                   transform=train_transform,
+                                                   target_transform=None,
+                                                   download=True)                                                
+minist_datasets_test = torchvision.datasets.MNIST(DATA_ROOT, train=False,
+                                                  transform=test_transform,
+                                                  target_transform=None,
+                                                  download=True)
+train_dataloader = torch.utils.data.DataLoader(
+    minist_datasets_train, batch_size=128, shuffle=True)
+test_dataloader = torch.utils.data.DataLoader(
+    minist_datasets_test, batch_size=128, shuffle=False)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# model = LinerModel(28, 28).to(device=device)
+model = CnnModel().to(device=device)
+optim = torch.optim.SGD(model.parameters(), lr=1e-2,
+                        momentum=0.9, weight_decay=1e-5)
+sch = torch.optim.lr_scheduler.StepLR(optim, 5, gamma=0.1)
+cri = torch.nn.CrossEntropyLoss()
+best_acc = 0.
+for j in range(15):
+    model.train()
+    predict_list = []
+    true_List = []
+    for i, batch in enumerate(train_dataloader):
+        out = model(batch[0].to(device))
+        loss = cri(out, batch[1].to(device))
+        predit, true = get_pair(out, batch[1].to(device))
+        predict_list.extend(predit)
+        true_List.extend(true)
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+        if i % 50 == 0:
+            acc = metrics.accuracy_score(predict_list, true_List)
+            print("epoch"+str(j)+" loss="+str('%.3f' % loss.item())+" acc: "+str(acc))
+            predict_list.clear()
+            true_List.clear()
+    test_acc = test(model, test_dataloader)
+    if test_acc >best_acc:
+        checkpoint = {
+            "model" : model.state_dict(),
+            "optim" : optim.state_dict(),
+            "sch" : sch.state_dict(),
+            "epoch" : j
+
+        }
+        torch.save(checkpoint,os.path.join(CKECKPOINT_ROOT,"best.pt"))
+    sch.step()
+    print("current lr = "+str(sch.get_lr()))
